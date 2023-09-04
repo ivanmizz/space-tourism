c7798b1bf87ee6ff0e915b86db0819d76827dd7d
Ivan Missana <ivanmissana@gmail.com>
2023-09-04 17:45:13 +0300

    fixed bugs

--- src/views/DestinationView.vue
+++ src/views/DestinationView.vue
@@ -1,8 +1,9 @@
 <script setup>
+  import destinationbackground from '@/assets/destination/background-destination-desktop.jpg';
 </script>
 
 <template>
-  <div class="bg-cyan-100">
+  <div class="bg-cover h-screen" :style="{ backgroundImage: 'url(' + destinationbackground + ')' }">
     <h1>This is the destination view page</h1>
   </div>
 </template>
